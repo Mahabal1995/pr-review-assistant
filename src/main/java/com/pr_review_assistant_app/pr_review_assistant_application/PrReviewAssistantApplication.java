a8f75d29cc0ef557b3585ffbae2046ceba748c54
Mahabal <mahabalshrivatsa95@gmail.com>
2025-08-04 22:24:30 +0530

    Initial Commit

---
+++ src/main/java/com/pr_review_assistant_app/pr_review_assistant_application/PrReviewAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.pr_review_assistant_app.pr_review_assistant_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrReviewAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrReviewAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pr_review_assistant_app/pr_review_assistant_application/PrReviewAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pr_review_assistant_app.pr_review_assistant_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrReviewAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
